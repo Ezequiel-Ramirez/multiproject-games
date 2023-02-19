cdb1f810e6beaf653e4f30917cec99abb40235e1
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2023-02-18 21:45:07 -0300

    api words

--- src/screens/WordsPerMinute/WordsPerMinute.tsx
+++ src/screens/WordsPerMinute/WordsPerMinute.tsx
@@ -1,31 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./WordsPerMinute.css";
 
-const WORDS = [
-    "abacus",
-    "abdomen",
-    "abdominal",
-    "abide",
-    "ability",
-    "abnormal",
-    "abolish",
-    "abortion",
-    "abound",
-    "about",
-    "above",
-    "abroad",
-    "absence",
-    "absent",
-    "absolute",
-    "absorb",
-];
-
 const WordsPerMinute = () => {
+    const [words, setWords] = useState<string[]>([]);
     const [characterCount, setCharacterCount] = useState(0);
     const [buffer, setBuffer] = useState("");
-    const [word, setWord] = useState(
-        () => WORDS[(Math.random() * WORDS.length) | 0]
-    );
+    const [word, setWord] = useState("");
     const [time, setTime] = useState(0);
     const [failTyping, setFailTyping] = useState(0);
     const isDesktop = window.innerWidth > 768;
@@ -34,11 +14,10 @@ const WordsPerMinute = () => {
         e.preventDefault();
         if (buffer === word) {
             setCharacterCount((characterCount) => characterCount + word.length);
-            setWord(WORDS[(Math.random() * WORDS.length) | 0]);
+            setWord(words[(Math.random() * words.length) | 0]);
         } else {
             setFailTyping(failTyping + 1);
         }
-
         setBuffer("");
     };
 
@@ -46,6 +25,16 @@ const WordsPerMinute = () => {
         setTime(60);
         setCharacterCount(0);
         setFailTyping(0);
+        callApiToGetWords();
+    };
+
+    const callApiToGetWords = async () => {
+        const response = await fetch(
+            "https://random-word-api.herokuapp.com/word?number=60"
+        );
+        const data = await response.json();
+        setWords(data);
+        setWord(data[(Math.random() * data.length) | 0]);
     };
 
     useEffect(() => {
@@ -56,6 +45,10 @@ const WordsPerMinute = () => {
             return () => clearTimeout(timeout);
         }
     }, [time]);
+
+    useEffect(() => {
+        callApiToGetWords();
+    }, []);
 
     return (
         <div className="containerWordsPerMinute">
@@ -73,23 +66,38 @@ const WordsPerMinute = () => {
                 </h2>
             )}
             <h3>Character typed: {characterCount}</h3>
-            {failTyping > 0 && <h3>Fail typing: <span style={{color: 'red'}}>{failTyping}</span></h3>}
+            {failTyping > 0 && (
+                <h3>
+                    Fail typing:{" "}
+                    <span style={{ color: "red" }}>{failTyping}</span>
+                </h3>
+            )}
             <h3 style={{ color: time < 10 ? "red" : "white" }}>
                 Remaining Time: {time}
             </h3>
             {time ? (
                 <form onSubmit={handleSubmit}>
                     <input
-                    style={{fontSize: isDesktop ? '1.5rem' : '0.8rem'}}
+                        style={{ fontSize: isDesktop ? "1.5rem" : "0.8rem" }}
                         type="text"
                         autoFocus
                         value={buffer}
                         onChange={(e) => setBuffer(e.target.value)}
                     />
-                    <button type="submit" style={{fontSize: isDesktop ? '1.5rem' : '0.8rem'}}>Submit</button>
+                    <button
+                        type="submit"
+                        style={{ fontSize: isDesktop ? "1.5rem" : "0.8rem" }}
+                    >
+                        Submit
+                    </button>
                 </form>
             ) : (
-                <button onClick={handlePlay} style={{fontSize: isDesktop ? '1.5rem' : '0.8rem'}}>Play</button>
+                <button
+                    onClick={handlePlay}
+                    style={{ fontSize: isDesktop ? "1.5rem" : "0.8rem" }}
+                >
+                    Play
+                </button>
             )}
         </div>
     );
